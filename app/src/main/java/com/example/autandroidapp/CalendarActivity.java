bd956e67de07b50dec72368454513d3cbbbfd958
Rachel Lee <49092422+rachellee2@users.noreply.github.com>
2019-04-30 12:41:43 +1200

    Update CalendarActivity.java

--- app/src/main/java/com/example/autandroidapp/CalendarActivity.java
+++ app/src/main/java/com/example/autandroidapp/CalendarActivity.java
@@ -49,10 +49,11 @@ public class CalendarActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
- master
+      master
 
-
-  Development
+=======
+  
+      Development
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
