77639b75719d8b071262171f649277b4c1bc5d37
jazmin <vjk0206@autuni.ac.nz>
2019-05-01 17:35:30 +1200

    added activities to ALL buttons

--- app/src/main/java/com/example/autandroidapp/childcare_button.java
+++ app/src/main/java/com/example/autandroidapp/childcare_button.java
@@ -1,17 +1,17 @@
 package com.example.autandroidapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class art_button extends AppCompatActivity {
+public class childcare_button extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_art_button);
+        setContentView(R.layout.activity_childcare_button);
 
         Intent intent = getIntent();
     }
--- app/src/main/java/com/example/autandroidapp/religious_button.java
+++ app/src/main/java/com/example/autandroidapp/religious_button.java
@@ -1,19 +1,17 @@
 package com.example.autandroidapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class art_button extends AppCompatActivity {
+public class religious_button extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_art_button);
-
-        Intent intent = getIntent();
+        setContentView(R.layout.activity_religious_button);
     }
 
     public void homeActivity(View view)
