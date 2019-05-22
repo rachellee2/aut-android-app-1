c0b8b68c06d3c621d3598f14e66b312300732bec
AlanaZupp <laniree@live.com>
2019-05-22 17:37:16 +1200

    Chatbot done with sound effects Main home page buttons have sounds and are rounded Start app icon has a picture. Will finish unit testing the chat bot later.

--- app/src/main/java/com/example/autandroidapp/MainActivity.java
+++ app/src/main/java/com/example/autandroidapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.autandroidapp;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,16 +10,22 @@ import androidx.appcompat.app.AppCompatActivity;
 //this class includes the activities of all the buttons on the homepage,
 //once the buttons are pressed, the activity will be executed.
 public class MainActivity extends AppCompatActivity {
+    private MediaPlayer main;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if(main == null) //Media players are space intensive so we only want one, create if not already created.
+        {
+            main = MediaPlayer.create(this,R.raw.blop_mark_diangelo);
+        }
     }
 
     //this method includes the activity of the chatbot button on the homepage,
     //once the chatbot button is pressed, the activity will be executed.
     public void chatbotActivity(View view) {
+        messagePop();
         Intent intent = new Intent(this, ChatbotActivity.class);
         startActivity(intent);
     }
@@ -26,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
     //this method includes the activity of the courses button on the homepage,
     //once the courses button is pressed, the activity will be executed.
     public void coursesActivity(View view) {
+        messagePop();
         Intent intent = new Intent(this, courses_button.class);
         startActivity(intent);
     }
@@ -33,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
     //this method includes the activity of the help button on the homepage,
     //once the help button is pressed, the activity will be executed.
     public void helpActivity(View view) {
+        messagePop();
         Intent intent = new Intent(this, help_button.class);
         startActivity(intent);
     }
@@ -40,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     //this method includes the activity of the services button on the homepage,
     //once the services button is pressed, the activity will be executed.
     public void servicesActivity(View view) {
+        messagePop();
         Intent intent = new Intent(this, services_button.class);
         startActivity(intent);
     }
@@ -47,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
     //this method includes the activity of the map button on the homepage,
     //once the map button is pressed, the activity will be executed.
     public void MapActivity(View view) {
+        messagePop();
         Intent intent = new Intent(this, MapActivity.class);
         startActivity(intent);
     }
@@ -54,8 +65,19 @@ public class MainActivity extends AppCompatActivity {
     //this method includes the activity of the calender button on the homepage,
     //once the calender button is pressed, the activity will be executed.
     public void CalendarActivity(View view) {
+        messagePop();
         Intent intent = new Intent(this, CalendarActivity.class);
         startActivity(intent);
     }
 
+    //to play the sound
+    public void messagePop()
+    {
+        getMain().start();
+    }
+
+    //Getter for the  media player
+    public MediaPlayer getMain() {
+        return main;
+    }
 }
