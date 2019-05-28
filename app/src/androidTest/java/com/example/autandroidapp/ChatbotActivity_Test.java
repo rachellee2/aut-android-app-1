ad1a0fd0064243905179177d5e5fe35d8ac2a249
AlanaZupp <laniree@live.com>
2019-05-28 21:18:42 +1200

    Fixed unit Testing

--- app/src/androidTest/java/com/example/autandroidapp/ChatbotActivity_Test.java
+++ app/src/androidTest/java/com/example/autandroidapp/ChatbotActivity_Test.java
@@ -1,60 +1,67 @@
-//package com.example.autandroidapp;
-//
-//import android.os.Looper;
-//import android.widget.TextView;
-//
-//import androidx.test.rule.ActivityTestRule;
-//
-//import org.junit.After;
-//import org.junit.Before;
-//import org.junit.Rule;
-//import org.junit.Test;
-//
-//import java.util.Objects;
-//
-//import static org.junit.Assert.*;
-//
-///**
-// * Class for Testing if the message received from the server is the same one echo'd
-// */
-//public class ChatbotActivity_Test
-//{
-//    @Rule
-//    public ActivityTestRule<ChatbotActivity> chatbotActivityActivityTestRule = new ActivityTestRule<ChatbotActivity>(ChatbotActivity.class);
-//    private ChatbotActivity chatbotActivity = null;
-//    TextView userMsg = null;
-//    TextView databaseMsg = null;
-//    boolean compare;
-//
-//    @Before
-//    //This method sets up data for testing
-//    public void setUp() throws Exception
-//    {
-//        chatbotActivity = chatbotActivityActivityTestRule.getActivity();
-//        userMsg = chatbotActivity.findViewById(R.id.editText);
-//        databaseMsg = chatbotActivity.findViewById(R.id.textMsgField);
-//        Looper.prepare();
-//    }
-//
-//    @After
-//    //This method frees data associated with test
-//    public void tearDown() throws Exception
-//    {
-//        userMsg = null;
-//        databaseMsg = null;
-//        chatbotActivity = null;
-//    }
-//
-//
-//    @Test
-//    //This method tests if the data sent and received from the database matches for the echo bot
-//    //Due to actionlistener not running in test after initialization this test wont run without
-//    //the TEST; code swapped out.
-//    public void sendMessage()
-//    {
-//        userMsg.setText("Hello");
-//        chatbotActivity.sendMessage(userMsg);
-//        compare = Objects.equals(userMsg.getText().toString(),databaseMsg.getText().toString());
-//        assertSame(compare, true);
-//    }
-//}
+package com.example.autandroidapp;
+
+import android.os.Looper;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.Objects;
+
+import static java.lang.Thread.sleep;
+import static org.junit.Assert.*;
+
+/**
+ * Class for Testing if the message received from the server is one of the selected
+ */
+public class ChatbotActivity_Test
+{
+    @Rule
+    public ActivityTestRule<ChatbotActivity> chatbotActivityActivityTestRule = new ActivityTestRule<ChatbotActivity>(ChatbotActivity.class);
+    private ChatbotActivity chatbotActivity = null;
+    TextView userMsg = null;
+    Boolean hit;
+    String[] option = {"Hi there, friend!","Hi!","Hey!","Hey there!","Good day!","Hello!","Greetings!"}; //all options from the msg hello
+
+    @Before
+    //This method sets up data for testing
+    public void setUp() throws Exception
+    {
+        chatbotActivity = chatbotActivityActivityTestRule.getActivity();
+        userMsg = chatbotActivity.findViewById(R.id.editText);
+        Looper.prepare();
+    }
+
+    @After
+    //This method frees data associated with test
+    public void tearDown()
+    {
+        userMsg = null;
+        chatbotActivity = null;
+        hit=null;
+    }
+
+
+    @Test
+    //This method tests if the data received from dialog flow matches any of their given response to Hello
+    //COMMENT OUT FOR TEST, both thread sleeps in Chatbot Activity to run
+    public void sendMessage() throws InterruptedException {
+        chatbotActivity.sendMessageTest("Hello");
+        sleep(3000); //to wait for the message to be received from dialogflow and inserted
+        ChatMsgList res = chatbotActivity.msgList.get(chatbotActivity.msgList.size()-1);
+        String result = res.getMsgContent();
+        for (int i = 0;i<option.length;i++) //compare if the result matches one of the options from dialog flow
+        {
+            if(result.equals(option[i]))
+            {
+                hit= true;
+            }
+        }
+        assertNotNull(hit);
+    }
+}
